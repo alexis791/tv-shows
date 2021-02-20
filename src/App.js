94f86c2567a377c9e622b82e137e19fb2b78a791
Alexis Delgado <dalexisbr@gmail.com>
2021-02-19 20:36:32 -0700

    Create media queries, install style componets, add atomic design structure.
    Media queries allow us what types of styles will be apply. This helps to re-design the content of our application and auto-adjust in our web browser window.
    Style componenets dependency allows to add css in javascript.
    Created components file and atomic desing files on it.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
-}
+const App = styled.section`
+	min-width: 360px;
+	max-width: 1900px;
+	width: 100%;
+	height: 100%;
+	margin: 0 auto;
+
+	/* Mobiles devices  */
+	@media (min-width: 320px) {
+		background-color: red;
+		&::after {
+			content: 'Mobiles devices';
+		}
+	}
+
+	/** Ipads, tablets */
+	@media (min-width: 480px) {
+		background-color: blue;
+		&::after {
+			content: 'Ipads, tablets';
+		}
+	}
+
+	/** Small screens, laptops */
+	@media (min-width: 780px) {
+		background-color: green;
+		&::after {
+			content: 'Smalls screens, laptops';
+		}
+	}
+
+	/** Desktop and Large screens */
+	@media (min-width: 1025px) {
+		background-color: orange;
+		&::after {
+			content: 'Desktop and large screens';
+		}
+	}
+
+	/** Extra large screens, TV */
+	@media (min-width: 1200px) {
+		background-color: yellowgreen;
+		&::after {
+			content: 'Extra large screens';
+		}
+	}
+`
 
 export default App
